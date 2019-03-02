3c28e29d99c786d5a2c974168a2075afd3be70a9
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2019-03-02 15:16:08 +0200

    Deploy

---
+++ precache-manifest.4c624b8ab911ef8fb085daecf2a081ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2b88b75649c8ce50988",
+    "url": "/static/js/main.d2b88b75.chunk.js"
+  },
+  {
+    "revision": "70f405e29daf7211ef8c",
+    "url": "/static/js/1.70f405e2.chunk.js"
+  },
+  {
+    "revision": "d2b88b75649c8ce50988",
+    "url": "/static/css/main.2515bdfd.chunk.css"
+  },
+  {
+    "revision": "72a3d9013581dc274d522ab04e7b5cd1",
+    "url": "/index.html"
+  }
+];
